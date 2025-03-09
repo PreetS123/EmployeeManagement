e3713f06ce8e47608538c21636d1188553faba11
preetys <preeti.sharma@axonichealth.com>
2025-03-09 14:39:40 +0530

    basic java setup

---
+++ emp_management/src/test/java/com/employee_management/emp_management/EmpManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee_management.emp_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
